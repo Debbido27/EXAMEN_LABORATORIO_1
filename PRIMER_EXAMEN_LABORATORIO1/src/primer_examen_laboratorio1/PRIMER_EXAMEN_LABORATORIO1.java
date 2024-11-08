90b52b713348e05534956e579c8ad657b0feb740
Debbido27 <Davidmedina272004@gmail.com>
2024-11-08 13:25:01 -0600

    Menu principal en do while

---
+++ PRIMER_EXAMEN_LABORATORIO1/src/primer_examen_laboratorio1/PRIMER_EXAMEN_LABORATORIO1.java
@@ -0,0 +1,27 @@
+
+package primer_examen_laboratorio1;
+
+import java.util.Scanner;
+
+
+public class PRIMER_EXAMEN_LABORATORIO1 {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner entrada = new Scanner(System.in);
+        int op;
+        
+        do{
+            System.out.print("****MENU EJERCICIOS****\n1.Piramide\n2.El mayor\n3.Canales\n4.Caracteres vocales\n5.Salir\nDigite la opcion que desea: ");
+            op = entrada.nextInt();
+            
+
+            
+            
+            
+        }while(op!=5);
+    }
+    
+}
